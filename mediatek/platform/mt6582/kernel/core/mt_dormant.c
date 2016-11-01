3e6c353849c3e888cb84e6f7f099d1fc2a074afc
Pablito2020 <socdelbarca72@gmail.com>
2016-11-01 16:18:34 +0100

    Update some files + toolchain

--- mediatek/platform/mt6582/kernel/core/mt_dormant.c
+++ mediatek/platform/mt6582/kernel/core/mt_dormant.c
@@ -585,8 +585,10 @@ extern void __inner_clean_dcache_L1(void);
 extern void __inner_clean_dcache_L2(void);
 extern void __inner_clean_dcache_all(void);
 
+#ifdef CONFIG_MTK_ETM
 extern void trace_stop_dormant(void);
 extern void trace_start_dormant(void);
+#endif
 
 //for save/restore breakpoint and watchpoint
 extern void save_dbg_regs(unsigned int data[]);
@@ -1511,10 +1513,11 @@ static void platform_restore_context(void)
         /* enable L2 invalidate when reset */
         reg_write(CA7_CACHE_CONFIG, reg_read(CA7_CACHE_CONFIG) & ~(1U << 4));
     }
-#if 0 /* FIXME early porting */
+#ifdef CONFIG_MTK_ETM
     trace_stop_dormant();
     trace_start_dormant();
 #endif    
+
     dormant_ret_flag[cpu_id] = 1;
 
     power_state[cpu_id] = STATUS_RUN;
